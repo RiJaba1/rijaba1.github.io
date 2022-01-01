8ceeb103a5f33279d94cffa48aee4dd26233fcc5
root <root@localhost.localdomain>
2022-01-01 14:06:43 +0100

    AddThings

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -2,15 +2,18 @@ var store = [{
         "title": "Canal encubierto WhatsApp",
         "excerpt":"En el siguiente artículo os mostraré la manera de mantener una conversaciñón privado entre dos personas sin que quede ningún tipo de registro en WhatsApp. Requerimientos Antes de iniciar tenemos que hacer las siguientes cosas: Instalar Heroku una herramienta que nos permitirá crear un servidor gratis go snap install heroku...","categories": ["Tutoriales","WhatsApp"],
         "tags": ["WhatsApp","Heroku","Canal encubierto"],
-        "url": "http://localhost:4000/canal-encubierto-whatsapp/",
-        "teaser":null},{
+        "url": "/canal-encubierto-whatsapp/",
+        "teaser": null
+      },{
         "title": "Herramienta whichSystem.sh",
         "excerpt":"Utilidad Esta herramienta nos permita averiguar el SO (Sistema Operativo) de una equipo sin hacer ruido y de manera instantánea. Para ello esta herramienta hace uso del comando ping disponible en todas las versiones de Linux de manera predeterminada. Uso de la herramienta ┌─[root@rijaba1-pc]─[~] └──╼ #git clone https://github.com/rijaba1/whichSystem.sh Clonando en...","categories": ["Herramientas"],
         "tags": ["Herramienta","Hacking","RiJaba1"],
-        "url": "http://localhost:4000/herramienta-whichsystem.sh/",
-        "teaser":null},{
+        "url": "/herramienta-whichsystem.sh/",
+        "teaser": null
+      },{
         "title": "Herramienta portDiscoverSH",
         "excerpt":"Utilidad Esta herramienta está diseñada para poder hacer un escaneo rápido y poco ruidoso a una IP que nosotros le asignemos. El enlace a GitHub para descargar esta utilidad lo teneis más abajo. Instalación La instalación de esta herramienta es muy sencilla dado que usa herramientas que ya vienen instaladas...","categories": ["Herramientas"],
         "tags": ["Herramienta","Hacking","RiJaba1"],
-        "url": "http://localhost:4000/herramienta-portDiscoverSH/",
-        "teaser":null}]
+        "url": "/herramienta-portDiscoverSH/",
+        "teaser": null
+      }]
